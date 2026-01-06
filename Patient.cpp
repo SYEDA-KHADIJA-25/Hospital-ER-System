70ea8c6cd9fce95b56cdef09055d07980aaea13b
SYEDA-KHADIJA-25 <syedaknaqvi9@gmail.com>
2026-01-06 15:40:47 +0500

    Delete Patient.cpp

--- Patient.cpp
+++
@@ -1,8 +0,0 @@
-#include "Patient.h"
-
-Patient::Patient()
-    : id(0), name(""), age(0), symptoms(""), priority(3) {}
-
-Patient::Patient(int i, const std::string& n, int a,
-                 const std::string& s, int p)
-    : id(i), name(n), age(a), symptoms(s), priority(p) {}
